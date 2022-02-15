547c3ec0d9f088e711496c38eb59832df31576c1
gabriel337 <g.david.lopezruiz@gmail.com>
2022-02-15 03:22:45 +0000

    add task

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * swap - swaps array elements
+ * @first_element: element to sort
+ * @second_element: element compare
+ */
+void swap(int *first_element, int *second_element)
+{
+	int temp = *first_element;
+	*first_element = *second_element;
+	*second_element = temp;
+}
+
+/**
+ * selection_sort - sorts array with selection sort
+ * @array: array being passed
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+
+		}
+			swap(&array[min_idx], &array[i]);
+			print_array(array, size);
+	}
+}
